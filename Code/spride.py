5a86bd78ca0c2cd11838b8fee8e13610b368136f
weiyangye <weiyangye@hotmail.com>
2017-03-23 16:32:11 +0800

    正常获取到页面内容

--- Code/spride.py
+++ Code/spride.py
@@ -2,40 +2,84 @@
 import os
 import sys
 import base64
+import time
 import requests
-
+import re
 from bs4 import BeautifulSoup
 
 
-
+host = 'http://www.xinfadi.com.cn'
+middle_url  = '/marketanalysis/0/list/'
+last_name = ".shtml"
 #soup = BeautifulSoup(html_doc,'html_parser')
+
 '''
 获取有多少页
 '''
+
+
 def get_gs_page_number():
     r = requests.get('http://www.xinfadi.com.cn/marketanalysis/0/list/1.shtml')
     soup= BeautifulSoup(r.content,'lxml')
-    print soup
+    #print soup.find_all('a')
+    #number = soup.find_all('a')[54:55] # 这个值是固定的，一段时间内是不太会改变
+    number = soup.find_all(title="尾页") # 这样更精确
+    #print number[0].get('href') # 获取以后的格式是： /marketanalysis/0/list/8758.shtml
+    last_page_uri = number[0].get('href')
+    #print last_page_uri
+    '''
+    使用正则表达式匹配出页码
+    '''
+    searchObj = re.search(r'[1-9]*\.', last_page_uri, re.M | re.I)
+    number =  searchObj.group().split('.')
+    number =  number[0]
+    number = int(number) #转换为整型
+    #print number
+    #print type(number)
+
+    return number
+    #print re.search(r'[1-9]*\.',last_page_uri)
+    #print (number[0].text).encode('utf-8')
+
 
 '''
-
+获取售货信息，示例如下：
+绿宝调和油
+5.76
+5.82
+5.87
+普通
+斤
+2016-01-01
 '''
+
+
 def get_goods_info():
-    #print "hahah"
-    r = requests.get("http://www.xinfadi.com.cn/marketanalysis/0/list/8737.shtml")
-    soup = BeautifulSoup(r.content, 'lxml')
+    number = get_gs_page_number()
+    print "统计出页数是：" + number.__str__()
 
-    '''
-     查找所有的tag，并列出,这部分挺重要
-    '''
-    #for tag in soup.find_all(True):
-    #    print(tag.name)
+    for i in range(1,number):
+        get_url= host + middle_url + i.__str__() + last_name
+        print get_url
+        #time.sleep(10) # 暂停10秒钟
+        for m in range(0,9):
+            time.sleep(1)
+            print '暂停中: ' + (10-m).__str__()
 
-    #table_con = soup.find_all('table')[1:2]
-    #print table_con
-    print len(soup.find_all('td'))
-    for table_d in soup.find_all('td')[16:176]:
-        print table_d.text
+        r = requests.get(get_url)
+        soup = BeautifulSoup(r.content, 'lxml')
+
+        '''
+         查找所有的tag，并列出,这部分挺重要
+        '''
+        #for tag in soup.find_all(True):
+        #    print(tag.name)
+
+        #table_con = soup.find_all('table')[1:2]
+        #print table_con
+        print len(soup.find_all('td'))
+        for table_d in soup.find_all('td')[16:176]:
+            print table_d.text
 
 if __name__ == "__main__":
     get_goods_info()
