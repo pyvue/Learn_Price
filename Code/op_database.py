877986097627535f9ae896c68d15c70dfac5d73d
weiyangye <weiyangye@hotmail.com>
2017-03-29 14:36:45 +0800

    处理蔬菜相关信息并导入到数据库中，vegetables_info.7z为蔬菜价格信息

--- Code/op_database.py
+++ Code/op_database.py
@@ -6,11 +6,16 @@ import time
 import mysqldb
 #import MySQLdb
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 #todo 添加异常处理模块
 
-
-def db_insert():
+'''
+数据库插入数据
+'''
+def db_insert(info_list):
     try:
         print "开始操作"
         conn = mysqldb.connect(host='localhost', user='root', passwd='', db='goods_db', port=3306)
@@ -21,10 +26,13 @@ def db_insert():
         构造信息的列表
         '''
         goods_info = ["'小白菜'", "'14.00'", "'12.13'", "'14.16'", "'普通'", "'斤'", "'2017-03-11'"]
+        goods_info = []
+        goods_info = info_list
+        print goods_info
         '''
         拼接sql字符串
         '''
-        sql = """INSERT INTO `goods_db`.`goods_info` (`GoodsName`, `Minivalence`, `MiddlePrice`, `HighestPrice`, `Specification`, `Unit`, `DataTime`) VALUES ({}, {}, {}, {}, {}, {}, {});"""
+        sql = """INSERT INTO `goods_db`.`goods_info` (`GoodsName`, `Minivalence`, `MiddlePrice`, `HighestPrice`, `Specification`, `Unit`, `DataTime`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}');"""
         sql = sql.format(goods_info[0],goods_info[1],goods_info[2],goods_info[3],goods_info[4],goods_info[5],goods_info[6])
         print sql
         #cur.execute("INSERT INTO `goods_db`.`goods_info` (`GoodsName`, `Minivalence`, `MiddlePrice`, `HighestPrice`, `Specification`, `Unit`, `DataTime`) VALUES ('小白菜', '14.00', '12.13', '14.16', '普通', '斤', '2017-03-21');")
@@ -36,9 +44,42 @@ def db_insert():
 
     except mysqldb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-    pass
+
+
+'''
+格式化数据
+'''
+
+def data_format():
+        f = open("goods_info.txt","r")
+        #line_number = len(f.readlines())  # 文件行数
+        #print line_number
+        i = 0
+        buff = f.readlines()
+        info_list = []
+        for line in buff:
+            info_list.append(line.strip('\n'))
+            if len(info_list) == 8 :
+               #print "信息长度为8了\n"
+               info_list.pop() # 删除最后一个空格
+               print info_list[0].decode("utf-8") # 解决编码问题
+               db_insert(info_list)    # 插入数据库
+               time.sleep(0.1)
+               info_list = [] # 清空列表
+            i += 1
+        print i
+        f.close()
+
+
+        #print os.path.getsize("goods_info.txt") # 文件大小
+        #line_number = len(f.readlines())                # 文件行数
+
+
+
+
 
 
 if __name__ == '__main__':
-    db_insert()
+    #db_insert()
+    data_format()
     print "main function"
--- Code/spride.py
+++ Code/spride.py
@@ -1,23 +1,48 @@
 #*.* coding=utf-8 *.*
 import os
-import sys
+
 import base64
 import time
 import requests
 import re
 from bs4 import BeautifulSoup
+import logging
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='run.log',
+                filemode='w') # 设置级别
+logging.warning('This is warning message')
+
 
 
 host = 'http://www.xinfadi.com.cn'
 middle_url  = '/marketanalysis/0/list/'
+middle_uri = '/marketanalysis/1/list/' #蔬菜价格的中间uri
 last_name = ".shtml"
 #soup = BeautifulSoup(html_doc,'html_parser')
 
 # todo 添加日志记录模块，用以排查出现的问题
-# todo 添加异常处理模块，网络链接出现问题的时候，自动进行任务，无人职守才行。
+# todo 添加异常处理模块，网络链接出现问题的时候，自动进行任务，无人职守才行。done
 # todo 添加数据库处理模块
-# todo 准备爬取蔬菜价格信息
-# todo 解决数据编码问题
+# todo 准备爬取蔬菜价格信息 done
+# todo 解决数据编码问题  半解决，还是有很多不明白的地方，python编码是一个坑
+# todo 编写查询数据库的功能代码
+
+'''
+bug 
+'''
+# todo 爬取完毕之后会从头开始爬，这个是一个问题，爬取完毕退出的问题
+# todo 最后一页可能不满足规定的 td ，解析页面的时候需要重新写
+
+
 
 '''
 获取有多少页
@@ -63,20 +88,29 @@ def get_gs_page_number():
 def get_goods_info():
     goods_info = open('vegetables_info.txt','ab+') #货物信息保存的文件
     number = get_gs_page_number()
-    print "统计出页数是：".decode("gbk2312") + number.__str__()
+    print sys.getdefaultencoding()
+    print "统计出页数是：".encode("gbk") + number.__str__()
 
-    for i in range(7506,number):
+    for i in range(0,number):
     #for i in range(1, 10):
-        get_url= host + middle_url + i.__str__() + last_name
+        get_url= host + middle_uri + i.__str__() + last_name
         print get_url
+        logging.warning(get_url)
         #time.sleep(10) # 暂停10秒钟
-        for m in range(0,5):
-            time.sleep(1)
-            print '暂停中: ' + (5-m).__str__()
-
-        r = requests.get(get_url)
-        soup = BeautifulSoup(r.content, 'lxml') #解析网页
-
+        '''
+        考虑缩短暂停时间
+        '''
+        #for m in range(0,2):
+        #    time.sleep(1)
+        #    print '暂停中: '.encode("gbk") + (5-m).__str__()
+        time.sleep(1)
+        try :
+            r = requests.get(get_url)
+        except Exception,e :
+            print e.args
+            logging.warning(e.args)
+        else:
+            soup = BeautifulSoup(r.content, 'lxml') #解析网页
         '''
          查找所有的tag，并列出,这部分挺重要
         '''
@@ -92,8 +126,17 @@ def get_goods_info():
             goods_info.write('\n')
 
     goods_info.close() #  爬取完毕，关闭文件
-    print "爬取结束。。。。"
+    print "爬取结束。。。。".encode("gbk")
 if __name__ == "__main__":
-    get_goods_info()
+    '''
+    添加异常处理
+    '''
+# todo 异常模块需要重新写
+    try:
+        get_goods_info()
+    except Exception ,e:
+        logging.warning(e.args)
+    finally:
+        get_goods_info()
     print "main function"
 
